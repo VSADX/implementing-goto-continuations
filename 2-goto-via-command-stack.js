f1832b27376d5e3f1de60b30625147890d911842
VSADX <81491645+VSADX@users.noreply.github.com>
2021-04-05 23:44:59 -0500

    Create 2-goto-via-command-stack.js

---
+++ 2-goto-via-command-stack.js
@@ -0,0 +1,47 @@
+function* stateable() {
+    const stack = []
+    let goto = 0
+    let stop = stack.length
+    const push = str => stack.push(str) && ++stop
+    const setGoto = num => goto = num
+    const setStop = num => exit = num
+    const state = {}
+    
+    while(!quit) {
+        let bag = {setGoto, setStop, state, push}
+        
+        console.log(bag)
+        
+        do var [exit, quit] = yield bag
+        while (!exit) exit = false
+    
+        console.log({goto, stop})
+
+        const cmds = stack.slice(goto, stop)
+        for(let cmd of cmds)
+            eval(cmd)
+        goto = stop
+    }
+    return state
+}
+
+/*
+
+const s = stateable()
+var {setGoto, setStop, state, push} = s.next([false, false]).value
+
+push("state.a = 3")
+push("state.b = 3")
+
+s.next([true, false])
+
+push("state.a += .5")
+
+s.next([true, false])
+setGoto(2)
+
+s.next([true, false])
+setGoto(2)
+
+const result = s.next([true, true]).value
+*/
