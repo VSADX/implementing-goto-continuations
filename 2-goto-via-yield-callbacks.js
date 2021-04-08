362673a87f807d9ca6c00953481a51e29a4a6d2f
VSADX <81491645+VSADX@users.noreply.github.com>
2021-04-07 21:50:57 -0500

    Create 2-goto-via-yield-callbacks.js

---
+++ 2-goto-via-yield-callbacks.js
@@ -0,0 +1,38 @@
+function useGoto(fn) {
+    if(!new.target) return new useGoto(fn)
+    this.stack = []
+    this.label = {}
+
+    this.goto = (function(name = [""]) {
+        let i = this.label[name[0]].position
+        while(i < this.stack.length) this.stack[i++]()
+    }).bind(this)
+    
+    for(const part of fn.call(this, this.goto)) {
+        if(typeof part === "function") {
+            const result = part.toString().match(/^\(?(\w+)\)? => /)
+            if(result) {
+                this.label[result[1]] = { value: part, position: this.stack.length }
+                this.stack.push(part)
+            }
+        }
+        part()
+    }
+}
+
+/*
+
+function* calc(goto) {
+  this.value = 0
+
+  yield add => this.value += 10 // this line runs 1 time
+  yield sub => this.value -= .5 // this line runs 3 times
+  
+  goto`sub`
+  goto`sub`
+
+  console.log("final value", this.value) // 8.5
+  return this.value
+}
+
+*/
